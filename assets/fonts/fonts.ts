ef395fa6f611d3cc0695f10c080399203af420f0
Rhut Virani <virani.rhut@gmail.com>
2025-06-05 17:41:50 -0600

    font files

--- assets/fonts/fonts.ts
+++ assets/fonts/fonts.ts
@@ -1,57 +1,42 @@
-// import localFont from 'next/font/local';
+import localFont from 'next/font/local';
 
-// // Load Mona Sans font
-// export const monaSans = localFont({
-//   src: [
-//     {
-//       path: '../assets/fonts/MonaSans-Regular.woff2',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//     {
-//       path: '../assets/fonts/MonaSans-Medium.woff2',
-//       weight: '500',
-//       style: 'normal',
-//     },
-//     {
-//       path: '../assets/fonts/MonaSans-SemiBold.woff2',
-//       weight: '600',
-//       style: 'normal',
-//     },
-//     {
-//       path: '../assets/fonts/MonaSans-Bold.woff2',
-//       weight: '700',
-//       style: 'normal',
-//     },
-//   ],
-//   variable: '--font-mona-sans',
-//   display: 'swap',
-// });
+// Load Mona Sans font
+export const monaSans = localFont({
+  src: [
+    {
+      path: './Mona_Sans/Static/MonaSans-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './Mona_Sans/Static/MonaSans-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './Mona_Sans/Static/MonaSans-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './Mona_Sans/Static/MonaSans-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-mona-sans',
+  display: 'swap',
+});
 
-// // Load Freight Display Pro font
-// export const freightDisplay = localFont({
-//   src: [
-//     {
-//       path: '../assets/fonts/FreightDisplay-Regular.woff2',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//     {
-//       path: '../assets/fonts/FreightDisplay-Medium.woff2',
-//       weight: '500',
-//       style: 'normal',
-//     },
-//     {
-//       path: '../assets/fonts/FreightDisplay-SemiBold.woff2',
-//       weight: '600',
-//       style: 'normal',
-//     },
-//     {
-//       path: '../assets/fonts/FreightDisplay-Bold.woff2',
-//       weight: '700',
-//       style: 'normal',
-//     },
-//   ],
-//   variable: '--font-freight-display',
-//   display: 'swap',
-// });
+// Load Dreaming font
+export const dreaming = localFont({
+  src: [
+    {
+      path: './Dreaming/Dreaming-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-dreaming',
+  display: 'swap',
+});
