df1450d615faed025ea0ffcbed1eb4fe562640da
Rhut Virani <virani.rhut@gmail.com>
2025-07-18 13:06:21 -0600

    update tina config

--- tina/config.tsx
+++ tina/config.tsx
@@ -6,18 +6,9 @@ import { navigation } from './collections/navigation';
 
 const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
-  branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-    process.env.HEAD!, // Netlify branch env
+  branch: process.env.VERCEL_GIT_COMMIT_REF || 'dev',
   token: process.env.TINA_TOKEN!,
   media: {
-    // If you wanted cloudinary do this
-    // loadCustomStore: async () => {
-    //   const pack = await import("next-tinacms-cloudinary");
-    //   return pack.TinaCloudCloudinaryMediaStore;
-    // },
-    // this is the config for the tina cloud media store
     tina: {
       publicFolder: 'public',
       mediaRoot: 'uploads',
