3bb127c089f52cb01d3be9e11aea268ab2bea65b
Rhut Virani <virani.rhut@gmail.com>
2026-03-24 12:41:37 -0600

    terms of use

---
+++ app/terms-of-use/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import client from '@/tina/__generated__/client';
+import Layout from '@/components/layout/layout';
+import { PageQuery, PageQueryVariables } from '@/tina/__generated__/types';
+import { notFound } from 'next/navigation';
+import TermsPage from '@/components/Terms/TermsPage';
+
+const TermsOfUse = async () => {
+  let data: PageQuery | undefined = undefined;
+  let query: string = '';
+  let variables: PageQueryVariables = { relativePath: `terms.mdx` };
+
+  try {
+    const res = await client.queries.page(variables);
+    query = res.query;
+    data = res.data;
+    variables = res.variables;
+  } catch (error) {
+    console.error(error);
+    console.error('Failed to load page data');
+  }
+
+  if (!data) return notFound();
+
+  return (
+    <Layout lightNavbar>
+      <TermsPage query={query} variables={variables} data={data} />
+    </Layout>
+  );
+};
+
+export default TermsOfUse;
